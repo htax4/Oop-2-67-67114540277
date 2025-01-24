1c870041e940689752216a1dd1fc6aebd1a784d3
htax4 <teerapon.po.67@ubu.ac.th>
2025-01-24 14:04:03 +0700

    สร้างคลาสแรกบน git repo1

---
+++ apply.py
@@ -0,0 +1,12 @@
+class Student(object):
+  stident = 0
+
+def __new__(self):
+  print('Student.__new__')
+
+def __init__(self):
+  print('Student.__new__')
+  self.gpa = 4.00
+
+def study_hard(self):
+  print('Sir, yes sir.')
